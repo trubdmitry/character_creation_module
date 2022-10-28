acc42a89b968a5b87ac9b3a77ebd29e20c107220
Трубкин Дмитрий <sregint@yandex.ru>
2022-10-28 22:23:11 +0400

    added Func: __str__

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ class Character:
         return (f'{self.name} применил специальное умение '
                 f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}".')
 
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BREIF_DESC_CHAR_CLASS}.'
+
 
 class Warrior(Character):
     pass
